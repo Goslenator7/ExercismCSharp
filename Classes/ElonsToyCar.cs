b795c74ff1309831b91fde6d3570f4c9e1cb8ef1
Ryan <rmc7394@gmail.com>
2023-12-11 21:19:42 +0000

    Update ElonsToyCar.cs
    Update some methods to expression body statements

--- Classes/ElonsToyCar.cs
+++ Classes/ElonsToyCar.cs
@@ -5,16 +5,10 @@ class RemoteControlCar
     private int _batteryLevel = 100;
     private int _distanceDriven = 0;
     
-    public static RemoteControlCar Buy()
-    {
-        return new RemoteControlCar();
-    }
-
-    public string DistanceDisplay()
-    {
-        return $"Driven {_distanceDriven} meters";
-    }
-
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+    
+    public string DistanceDisplay() => $"Driven {_distanceDriven} meters";
+    
     public string BatteryDisplay()
     {
         if (_batteryLevel > 0)
@@ -23,7 +17,7 @@ class RemoteControlCar
         }
         return $"Battery empty";
     }
-
+    
     public void Drive()
     {
         if (_batteryLevel > 0)
